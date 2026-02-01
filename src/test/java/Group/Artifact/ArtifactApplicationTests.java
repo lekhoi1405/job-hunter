350b3296c7179ab7ff596be57def512147741ef4
koi <nuyenkhoi2407@gmail.com>
2026-02-01 22:56:08 +0700

    first commit

---
+++ src/test/java/Group/Artifact/ArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package Group.Artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
